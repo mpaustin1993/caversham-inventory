2a1d58b4e99d29a1aee46667cb49b3ae2ca9c32d
Michael Austin <mpaustin1993@gmail.com>
2025-12-21 09:01:57 -0600

    enabled CORS for backend

--- inventory-api/routes/configure.go
+++ inventory-api/routes/configure.go
@@ -6,8 +6,15 @@ import (
 	"net/http"
 )
 
+func enableCORS(w http.ResponseWriter) {
+    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
 func Configure(db *sql.DB) {
 	http.HandleFunc("/inventory", func(w http.ResponseWriter, r *http.Request) {
+		enableCORS(w)
 		switch r.Method {
 		case http.MethodGet:
 			handlers.GetInventory(db, w, r)
@@ -17,6 +24,7 @@ func Configure(db *sql.DB) {
 	})
 
 	http.HandleFunc("/inventory/{id}", func(w http.ResponseWriter, r *http.Request) {
+		enableCORS(w)
 		switch r.Method {
 		case http.MethodPut:
 			handlers.UpdateItem(db, w, r)
