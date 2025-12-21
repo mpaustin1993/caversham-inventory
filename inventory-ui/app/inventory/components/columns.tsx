8e5b79a4deb8efa7046ef6f223a510a888faa8ff
Michael Austin <mpaustin1993@gmail.com>
2025-12-21 10:45:23 -0600

    Added Actions as DropDown Menu

--- inventory-ui/app/inventory/components/columns.tsx
+++ inventory-ui/app/inventory/components/columns.tsx
@@ -5,6 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { Item } from "../../../lib/types";
 
+import { MoreHorizontal } from "lucide-react"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 export const columns: ColumnDef<Item>[] = [
     {
     accessorKey: "category",
@@ -52,5 +64,28 @@ export const columns: ColumnDef<Item>[] = [
   {
     accessorKey: "note",
     header: "Note",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
